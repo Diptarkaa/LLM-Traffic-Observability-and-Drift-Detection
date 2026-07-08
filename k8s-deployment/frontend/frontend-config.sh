0be085641e69a16d37b0539020f0176f48cab929
pugr_AKAM <pugr@akamai.com>
2026-07-08 12:35:40 +0530

    ORGWAAP-7420: feat(llm-frontend): add a frontend to send request and parse response in OpenAI format

--- k8s-deployment/frontend/frontend-config.sh
+++ k8s-deployment/frontend/frontend-config.sh
@@ -1,1 +1,3 @@
-kubectl -n llmg create configmap frontend-html --from-file=frontend/index.html
+kubectl -n llmg create configmap frontend-html \
+  --from-file=index.html=frontend/index.html \
+  --from-file=llm.html=frontend/llm.html
